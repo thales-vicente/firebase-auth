cfc5b83cddfdc85b6911b59fc0d71eb6c45921ff
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-03-24 11:18:53 -0300

    feat: Implement Login and Signup UI
    This commit introduces the LoginActivity and a new UI for user login and signup.

--- app/src/main/java/com/example/firebaseauth/IntroActivity.kt
+++ app/src/main/java/com/example/firebaseauth/IntroActivity.kt
@@ -1,20 +1,23 @@
 package com.example.firebaseauth
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.firebaseauth.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnInscrever.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
+
     }
 }
